e1fef084d00bfcceaad8229397dd19756c47eeea
Strawson Design <james@strawsondesign.com>
2016-03-09 17:09:21 -0800

    cleanup main comments

--- main.m
+++ main.m
@@ -9,10 +9,10 @@ format short
 n=4; % square matrix height/width
 
 %% recursive depth
-% 3 -> fast
-% 4 -> medium
+% 3 -> fast, usually gets the 2048 tile
+% 4 -> medium, usually gets the 8192 tile
 % 5 -> painfully slow
-recursive_depth = 5;
+recursive_depth = 4;
 
 %% absolute starting points
 start = [ 0 0 0 0;
@@ -21,23 +21,23 @@ start = [ 0 0 0 0;
           0 0 0 0];
 
 %% for testing to 8k
-start = [ 8         32        16          4;
-          0          0        4096        128;
-          0          2         0           0;
-          0          0         0           2];
-      
+% start = [ 8         32        16          4;
+%           0          0        4096        128;
+%           0          2         0           0;
+%           0          0         0           2];
+%       
 %% for testing to 16k
-start = [ 8         32        16          4;
-          0          0        8192        128;
-          0          2         0           0;
-          0          0         0           2];
+% start = [ 8         32        16          4;
+%           0          0        8192        128;
+%           0          2         0           0;
+%           0          0         0           2];
       
       
 %% best so far!
-% start = [ 4          16       128         2;
-%          32          64       256         4;
-%          1024       4096      512         32;
-%           2           8      16384        16];
+%         [  8          16          64           4;
+%           32         512        1024          16;
+%         8192         256        2048          32;
+%            4        4096           8           2];
 
 
 %% make sure the matrix and score uses ints not floats
@@ -55,7 +55,7 @@ for i=1:100000
     
     % lower recursive depth again if you are not close to a new big number
     % this is done by counting unique values
-    uniques = size(unique(new(:)))
+    uniques = size(unique(new(:)));/
     if(uniques(1)<=9)
         depth = depth-1;
     end
